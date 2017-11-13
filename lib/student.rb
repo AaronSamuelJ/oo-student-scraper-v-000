66baa8d86d264586d56fa2a059d493e78a0b4d49
Aaron Samuel <aasamuelj@gmail.com>
2017-11-13 00:46:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,14 +4,14 @@ class Student
 
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
   end
 
   def add_student_attributes(attributes_hash)
-  
+
   end
 
   def self.all
